7836a7afd5b7453bc0af73967b9732aa841f8718
sarl23 <sergioandresrojasleon@gmail.com>
2016-12-10 22:52:39 -0500

    descargo 2 ser

---
+++ app/src/main/java/com/jimmy/hospitalsapp/activities/Register_PatientAct.java
@@ -0,0 +1,15 @@
+package com.jimmy.hospitalsapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jimmy.hospitalsapp.R;
+
+public class Register_PatientAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register__patient);
+    }
+}
