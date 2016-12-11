19507ed00d310e62a88a93174cf9057d2068be57
jimmyale3102 <jimmyale3102@gmail.com>
2016-12-10 23:35:10 -0500

    actualizacion

--- app/src/main/java/com/jimmy/hospitalsapp/activities/BeginActivity.java
+++ app/src/main/java/com/jimmy/hospitalsapp/activities/BeginActivity.java
@@ -1,9 +1,14 @@
 package com.jimmy.hospitalsapp.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.jimmy.hospitalsapp.R;
+import com.jimmy.hospitalsapp.logic.ManagementUser;
 
 public class BeginActivity extends AppCompatActivity {
 
@@ -12,6 +17,21 @@ public class BeginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_begin);
 
+        EditText etUserName = (EditText) findViewById(R.id.etUserName);
+        ImageButton btnLogin = (ImageButton) findViewById(R.id.btnLogin);
+        ManagementUser mgUser = new ManagementUser(this);
+        EditText etRegistrer = (EditText) findViewById(R.id.etRegistrer);
+
+        etRegistrer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mgUser.validateUserName(etUserName.getText())) {
+                    Intent intent = new Intent(BeginActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
     }
 
 }
